9a2f26c5083e4d955ac5a3eac00b359a6c56ac27
Daniel Kudernatsch <d.kudernatsch@outlook.com>
2017-10-31 22:38:51 +0100

    Added Config Factory to more easily create YamtConfig

---
+++ yamtlib/src/main/java/org/itp1/yamtlib/config/ConfigFactory.java
@@ -0,0 +1,28 @@
+package org.itp1.yamtlib.config;
+
+import org.itp1.errors.YamtException;
+
+import java.util.Arrays;
+
+public class ConfigFactory {
+
+    private static IncompleteYamtConfig safeGenerate(ConfigGenerator cg) {
+        try {
+            return cg.generate();
+        } catch (YamtException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    public YamtConfig generate(ConfigGenerator... generators) throws YamtException {
+        try {
+            return Arrays.stream(generators)
+                    .map(ConfigFactory::safeGenerate)
+                    .reduce(new IncompleteYamtConfig(), IncompleteYamtConfig::merge)
+                    .verify();
+        } catch (RuntimeException e) {
+            throw new YamtException(e);
+        }
+    }
+}
+
--- yamtlib/src/test/java/org/itp1/yamtlib/config/TestConfig.java
+++ yamtlib/src/test/java/org/itp1/yamtlib/config/TestConfig.java
@@ -3,6 +3,7 @@ package org.itp1.yamtlib.config;
 import org.itp1.errors.YamtException;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -61,6 +62,58 @@ public class TestConfig {
         try {
             YamtConfig realConfig = config.verify();
             Assert.fail("Should fail, not complete Config");
+        } catch (YamtException e) {
+            Assert.assertEquals(true, true);
+        }
+    }
+
+    @Test
+    public void testConfigFactoryNotThrowsWhenAllOk() throws YamtException.ConfigException {
+
+        IncompleteYamtConfig inc1 = new IncompleteYamtConfig();
+        inc1.mergeFormat("Hello");
+
+        IncompleteYamtConfig inc2 = new IncompleteYamtConfig();
+        inc2.mergeMusicDir(Paths.get("."));
+
+        IncompleteYamtConfig inc3 = new IncompleteYamtConfig();
+        inc3.mergeOutDir(Paths.get(".."));
+
+        ConfigGenerator cfgGen1 = Mockito.mock(ConfigGenerator.class);
+        Mockito.when(cfgGen1.generate()).thenReturn(inc1);
+
+        ConfigGenerator cfgGen2 = Mockito.mock(ConfigGenerator.class);
+        Mockito.when(cfgGen2.generate()).thenReturn(inc2);
+
+        ConfigGenerator cfgGen3 = Mockito.mock(ConfigGenerator.class);
+        Mockito.when(cfgGen3.generate()).thenReturn(inc3);
+
+        try {
+            YamtConfig ycfg = new ConfigFactory().generate(cfgGen1, cfgGen2, cfgGen3);
+        } catch (YamtException e) {
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testConfigFactoryAbortWhenException() throws YamtException {
+        IncompleteYamtConfig inc1 = new IncompleteYamtConfig();
+        inc1.mergeFormat("Hello");
+
+        IncompleteYamtConfig inc2 = new IncompleteYamtConfig();
+        inc2.mergeMusicDir(Paths.get("."));
+
+        ConfigGenerator cfgGen1 = Mockito.mock(ConfigGenerator.class);
+        Mockito.when(cfgGen1.generate()).thenReturn(inc1);
+
+        ConfigGenerator cfgGen2 = Mockito.mock(ConfigGenerator.class);
+        Mockito.when(cfgGen2.generate()).thenReturn(inc2);
+
+        ConfigGenerator cfgGen3 = Mockito.mock(ConfigGenerator.class);
+        Mockito.when(cfgGen3.generate()).thenThrow(new YamtException.ConfigException("Test Error"));
+        try {
+            YamtConfig ycfg = new ConfigFactory().generate(cfgGen1, cfgGen2, cfgGen3);
+            Assert.fail("Did not throw exception");
         } catch (YamtException e) {
             Assert.assertEquals(true, true);
         }
