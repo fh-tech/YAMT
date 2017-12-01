aab224394d3b37c6634b29e1fc42ddf78d3243ea
Matt <legat.matt@gmail.com>
2017-12-01 19:48:34 +0100

    added test example

--- yamtlib/src/main/java/org/itp1/yamtlib/files/FileHandler.java
+++ yamtlib/src/main/java/org/itp1/yamtlib/files/FileHandler.java
@@ -1,4 +1,19 @@
 package org.itp1.yamtlib.files;
+import java.io.*;
 
 public class FileHandler {
+
+    public static void printFilesFromFolder(String path){
+        File folder = new File(path);
+        File[] listOfFiles = folder.listFiles();
+
+        for (final File fileEntry : listOfFiles) {
+            if (fileEntry.isFile()) {
+                System.out.println("File " + fileEntry.getName());
+            } else if (fileEntry.isDirectory()) {
+                System.out.println("Directory " + fileEntry.getName());
+            }
+        }
+    }
+
 }
