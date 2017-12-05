4552ff06abac6f767804d8f629527971e4d4e76c
Daniel Kudernatsch <d.kudernatsch@outlook.com>
2017-12-05 20:46:08 +0100

    Added support for escaping template brackets Added test cases

--- yamtlib/src/main/java/org/itp1/yamtlib/format/MusicFormatter.java
+++ yamtlib/src/main/java/org/itp1/yamtlib/format/MusicFormatter.java
@@ -14,41 +14,58 @@ public class MusicFormatter {
         this.formatString = formatString;
     }
 
+
+    /***
+     * Generates a new file name for a given music file with the <code>formatString</code> used to construct this {@link MusicFormatter}
+     * @param music A YamtMusic object for which a new file name is generated
+     * @return the new filename relative to the output directory as String
+     * @throws YamtException.FormatException when the given format string is invalid or if the required metadata is not present
+     */
     public String format(YamtMusic music) throws YamtException.FormatException {
         try {
-            return partialFormat(formatString, music);
-
-        }catch (YamtException.FormatException fe){
+            return partialFormat(formatString, music)
+                    .replaceAll("\\\\\\{", "{")
+                    .replaceAll("\\\\}", "}");
+        } catch (YamtException.FormatException fe) {
             throw fe;
-        }catch(YamtException e){
-            throw new YamtException.FormatException("Unable to format file ["+music.getFile()+"]", e);
+        } catch (YamtException e) {
+            throw new YamtException.FormatException("Unable to format file [" + music.getFile() + "]", e);
         }
     }
 
     private String partialFormat(String remainingFormat, YamtMusic music) throws YamtException {
 
         //get both opening and closing Brackets in remaining String
-        int iOpenBracket = remainingFormat.indexOf('{');
+        int iOpenBracket = indexOfNotEscaped(0, '{', remainingFormat);
         if (iOpenBracket < 0) return "";
 
-        int iCloseBracket = remainingFormat.indexOf('}', iOpenBracket);
+        int iCloseBracket = indexOfNotEscaped(0, '}', remainingFormat);
         if (iCloseBracket < 0) return "";
 
         //between brackets is the identifier
-        String metaDataIdentifier = remainingFormat.substring(iOpenBracket+1, iCloseBracket);
+        String metaDataIdentifier = remainingFormat.substring(iOpenBracket + 1, iCloseBracket);
         String metaData = music.getTag(WantedKey.fromTemplateString(metaDataIdentifier));
 
-        if(metaData != null && !metaData.equals("")) {
+        if (metaData != null && !metaData.equals("")) {
             //this method with the string after the closing bracket
             return remainingFormat.substring(0, iOpenBracket) +
                     metaData +
-                    partialFormat(remainingFormat.substring(iCloseBracket+1), music);
+                    partialFormat(remainingFormat.substring(iCloseBracket + 1), music);
 
         } else {
             throw new YamtException.FormatException(
                     "Unable to generate new name for file ["
                             + music.getFile().getPath() +
-                            "]\n Missing metadata: ["+metaDataIdentifier+"]");
+                            "]\n Missing metadata: [" + metaDataIdentifier + "]");
+        }
+    }
+
+    private static int indexOfNotEscaped(int from, char of, String s){
+        int index = s.indexOf(of, from);
+        if(index > 0 && s.charAt(index-1) == '\\'){
+            return indexOfNotEscaped(index+1, of, s);
+        }else{
+            return index;
         }
     }
 }
--- yamtlib/src/test/java/org/itp1/yamtlib/TestFormat.java
+++ yamtlib/src/test/java/org/itp1/yamtlib/TestFormat.java
@@ -1,9 +1,14 @@
 package org.itp1.yamtlib;
 
+import org.itp1.yamtlib.errors.YamtException;
 import org.itp1.yamtlib.format.MusicFormatter;
+import org.itp1.yamtlib.music.WantedKey;
 import org.itp1.yamtlib.music.YamtMusic;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.io.File;
 import java.nio.file.CopyOption;
@@ -11,23 +16,68 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import static org.mockito.Mockito.when;
+
 
 public class TestFormat {
+
+    private static YamtMusic wellTagged;
+    private static YamtMusic throwsException;
+    private static YamtMusic empty;
+
+    @BeforeClass
+    public static void setup(){
+        wellTagged = Mockito.mock(YamtMusic.class);
+        throwsException = Mockito.mock(YamtMusic.class);
+        try {
+            when(wellTagged.getTag(WantedKey.ALBUM))
+                    .thenReturn("aLBum");
+
+            when(wellTagged.getTag(WantedKey.ARTIST))
+                    .thenReturn("ArtIsT");
+
+            when(wellTagged.getTag(WantedKey.TITLE))
+                    .thenReturn("tiTle");
+
+            when(throwsException.getTag(Mockito.any()))
+                    .thenThrow(new YamtException.MusicException("Mock Exception"));
+
+        }catch (Exception e){}
+    }
+
 
 
     @Test
     public void testFormatting() throws Exception {
         MusicFormatter mf = new MusicFormatter("{artist}/{title}");
-        YamtMusic music = new YamtMusic(getClass().getResource("/test.mp3").getFile());
-        String newName = mf.format(music);
-        File f = new File(newName);
-
-        Files.copy(Paths.get(getClass().getResource("/test.mp3").toURI()), Paths.get(newName+".mp3"));
-
-        Assert.assertEquals("", newName);
-
+        String newName = mf.format(wellTagged);
+        Assert.assertEquals("ArtIsT/tiTle", newName);
     }
 
-    //TODO: write more tests
+    @Test
+    public void testEscapeBracket() throws Exception{
+        MusicFormatter mf = new MusicFormatter("\\{\\}abc{album}/{title}");
+        String newName = mf.format(wellTagged);
+        Assert.assertEquals("{}abcaLBum/tiTle", newName);
+    }
 
+    @Test
+    public void testFormatStringWithoutTemplateShouldReturnEmptyString() throws Exception{
+        String testString  = "hello/wor\\{ld/ab\\}c";
+        MusicFormatter mf = new MusicFormatter(testString);
+        String newName = mf.format(Mockito.mock(YamtMusic.class));
+        Assert.assertEquals("", newName);
+    }
+
+    @Test(expected = YamtException.FormatException.class)
+    public void shouldRethrow() throws YamtException {
+        MusicFormatter mf = new MusicFormatter("{artist}/{title}");
+        mf.format(throwsException);
+    }
+
+    @Test(expected = YamtException.FormatException.class)
+    public void shouldThrowExceptionWhenMetaTagIsNotSet() throws YamtException{
+        MusicFormatter mf = new MusicFormatter("{hello}/world");
+        mf.format(empty);
+    }
 }
